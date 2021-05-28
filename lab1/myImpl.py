eba7c52405dc8163ac764f98c63b242d348451c3
Alpha-Girl <alphaboy@mail.ustc.edu.cn>
2021-05-28 09:49:08 +0800

    minimax pass

--- lab1/myImpl.py
+++ lab1/myImpl.py
@@ -27,10 +27,12 @@ Returns True if and only if the state is a valid goal state.
 
 problem.getChildren(state)
 For a given state, this should return a list of tuples, (next_state,
-step_cost), where 'next_state' is a child to the current state, 
+step_cost), where 'next_state' is a child to the current state,
 and 'step_cost' is the incremental cost of expanding to that child.
 
 """
+
+
 def myDepthFirstSearch(problem):
     visited = {}
     frontier = util.Stack()
@@ -45,8 +47,8 @@ def myDepthFirstSearch(problem):
             while prev_state != None:
                 solution.append(prev_state)
                 prev_state = visited[prev_state]
-            return solution[::-1]                
-        
+            return solution[::-1]
+
         if state not in visited:
             visited[state] = prev_state
 
@@ -55,15 +57,51 @@ def myDepthFirstSearch(problem):
 
     return []
 
+
 def myBreadthFirstSearch(problem):
-    # YOUR CODE HERE
-    util.raiseNotDefined()
+
+    visited = {}
+    frontier = util.Queue()
+
+    frontier.push((problem.getStartState(), None))
+
+    while not frontier.isEmpty():
+        state, prev_state = frontier.pop()
+        if problem.isGoalState(state):
+            solution = [state]
+            while prev_state != None:
+                solution.append(prev_state)
+                prev_state = visited[prev_state]
+            return solution[::-1]
+        if state not in visited:
+            visited[state] = prev_state
+            for next_state, step_cost in problem.getChildren(state):
+                frontier.push((next_state, state))
     return []
 
+
 def myAStarSearch(problem, heuristic):
-    # YOUR CODE HERE
-    util.raiseNotDefined()
+    visited = {}
+    frontier = util.PriorityQueue()
+    frontier.update((problem.getStartState(), None, 0),
+                    heuristic(problem.getStartState()))
+
+    while not frontier.isEmpty():
+        state, prev_state, cost = frontier.pop()
+        if problem.isGoalState(state):
+            solution = [state]
+            while prev_state != None:
+                solution.append(prev_state)
+                prev_state = visited[prev_state]
+            return solution[::-1]
+        if state not in visited:
+            visited[state] = prev_state
+            for next_state, step_cost in problem.getChildren(state):
+                h_n = heuristic(next_state)
+                frontier.update(
+                    (next_state, state, cost+step_cost), cost+step_cost+h_n)
     return []
+
 
 """
 Game state has 4 methods we can use.
@@ -81,26 +119,44 @@ state.evaluateScore()
 Return the score of the state. We should maximum the score for the desired agent.
 
 """
+
+
 class MyMinimaxAgent():
 
     def __init__(self, depth):
         self.depth = depth
 
     def minimax(self, state, depth):
-        if state.isTerminated():
-            return None, state.evaluateScore()        
+        if state.isTerminated() :
+            return None, state.evaluateScore()
 
-        best_state, best_score = None, -float('inf') if state.isMe() else float('inf')
+        best_state, best_score = None, - \
+            float('inf') if state.isMe() else float('inf')
 
         for child in state.getChildren():
-            # YOUR CODE HERE
-            util.raiseNotDefined()
-        
+            if state.isMe():
+                _,tmp_score = self.minimax(child, depth-1)
+                if tmp_score > best_score:
+                    best_score = tmp_score
+                    best_state = child
+            else:
+                if child.isMe() and depth==0 :
+                    tmp_score=child.evaluateScore()
+                    if tmp_score<best_score:
+                        best_score=tmp_score
+                        best_state=child
+                else:
+                    _,tmp_score =self.minimax(child,depth)
+                    if tmp_score<best_score:
+                        best_score=tmp_score
+                        best_state=child
         return best_state, best_score
 
     def getNextState(self, state):
         best_state, _ = self.minimax(state, self.depth)
         return best_state
+
+
 
 class MyAlphaBetaAgent():
 
--- lab1/search/grading.py
+++ lab1/search/grading.py
@@ -15,6 +15,7 @@
 "Common code for autograders"
 
 import cgi
+import html
 import time
 import sys
 import json
@@ -291,7 +292,7 @@ to follow your instructor's guidelines to receive credit on your project.
         if self.mute: util.unmutePrint()
         print('*** ' + message)
         if self.mute: util.mutePrint()
-        message = cgi.escape(message)
+        message = html.escape(message)
     self.messages[self.currentQuestion].append(message)
 
   def addMessageToEmail(self, message):
