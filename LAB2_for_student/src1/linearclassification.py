689e52b1d0da16d6102c26bfd117f5147e78c8bc
Alpha-Girl <alphaboy@mail.ustc.edu.cn>
2021-07-14 16:39:39 +0800

    code style

--- LAB2_for_student/src1/linearclassification.py
+++ LAB2_for_student/src1/linearclassification.py
@@ -14,7 +14,7 @@ class LinearClassification:
     epochs: 更新迭代的次数
     '''
 
-    def __init__(self, lr=0.0005, Lambda=0.001, epochs=1000):
+    def __init__(self, lr=0.05, Lambda=0.001, epochs=1000):
         self.lr = lr
         self.Lambda = Lambda
         self.epochs = epochs
@@ -22,10 +22,11 @@ class LinearClassification:
     '''根据训练数据train_features,train_labels计算梯度更新参数W'''
 
     def fit(self, train_features, train_labels):
+        # 参数weight，bias初始化
         self.weight = np.array(np.random.rand(
             train_features.shape[1], train_labels.shape[1]))
-        # print("w:",self.weight.shape)
         self.bias = np.zeros(train_labels.shape[1])
+        # 计算更新时用的矩阵
         x_T = train_features.transpose()
         a = x_T.dot(train_features)
         a = a + self.Lambda * \
@@ -33,24 +34,16 @@ class LinearClassification:
         a = a.transpose()
         a = np.linalg.inv(a)
         a = a.dot(x_T)
-        # print("a:"+a.shape)
+        # 训练 迭代
         for i in range(self.epochs):
+            # 预测
             y_pred = self.predict(train_features)
-            # print(y_pred.shape)
+            # 计算梯度
             d_w = a@(y_pred - train_labels)
-            # print(d_w.shape)
             d_b = np.mean(y_pred-train_labels)
-            # print(d_b.shape)
+            # 更新参数
             self.weight = self.weight - self.lr * d_w
             self.bias = self.bias - self.lr * d_b
-
-        '''
-        x_T = train_features.transpose()
-        a = x_T @ train_features + self.Lambda @ \
-            np.identity(train_features.shape[1], dtype=np.float32)
-        a = a.transpose()
-        a = np.linalg.inv(a)@x_T@train_labels
-        W = W - self.Lambda@a'''
 
     '''根据训练好的参数对测试数据test_features进行预测，返回预测结果
     预测结果的数据类型应为np数组，shape=(test_num,1) test_num为测试数据的数目'''
@@ -58,9 +51,8 @@ class LinearClassification:
     def predict(self, test_features):
         y_pred_proba = test_features.dot(self.weight)+self.bias
         y_pred = y_pred_proba
-        # print(y_pred_proba.shape)
+        # 预测
         for k in range(y_pred_proba.shape[0]):
-
             y_pred[k][0] = math.floor(y_pred_proba[k][0])
         return y_pred
 
